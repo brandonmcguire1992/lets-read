c55a13ca35dda78753cab7b0dd98fa4d9eca16a0
Brandon McGuire <brandonmcguire1992@gmail.com>
2020-10-11 12:45:32 -0500

    addes mutations and queries

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Book Search Engine Starter Code
+# Google Books Search
+
+## [Click Here to View the Project]()
+
+<img src="assets/images/lets-read-image.jpg">
+
+## Description
+
+Full-stack MERN app that allows a user to signup, login, search, and save a book to access at another time.
+
+## Questions
+
+Vist me at GitHub [here](https://github.com/brandonmcguire1992)
+
+Or [email](mailto:brandonmcguire1992@gmail.com) me.
--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,13 +15,27 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers,
+    // make updated request object (with .user) available to the resolvers as the context.
+    context: authMiddleware
+});
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app })
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// wildcard route 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`));
 });
